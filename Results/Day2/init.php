7ddbf809e16b56112ced7fb142e18dc1db73903a
Emilio Rodriguez <emilio.rodriguez@sngular.com>
2019-12-04 13:55:00 +0100

    add results scripts

--- Results/Day2/init.php
+++ Results/Day2/init.php
@@ -1,10 +1,20 @@
 <?php
-require __DIR__.'/../../../vendor/autoload.php';
+require '../config.php';
 
 use AdventOfCode\Domain\Entity\Computer;
 
 $inputs = file(dirname(__FILE__) . "/inputs_day_2.txt");
-$instructions = implode(",", $inputs);
-$computer = Computer::createFromMemory($instructions);
+$memory = implode(",", $inputs);
+$computer = Computer::createFromMemoryString($memory);
 
-echo "Resultado parte 1 " . $computer->runIntCodeProgram() . PHP_EOL;
+echo "Resultado parte 1 " . $computer->runIntCodeProgramWithInputs(12, 2) . PHP_EOL;
+
+$computer = Computer::createFromMemoryString($memory);
+for ($noun = 0; $noun < 100; $noun++) {
+  for ($verb = 0; $verb < 100; $verb++) {
+    if($computer->runIntCodeProgramWithInputs($noun, $verb)==19690720){
+      echo "Resultado parte 2 " . (100 * $noun + $verb);
+      die();
+    }
+  }
+}
--- tests/Entity/ComputerTest.php
+++ tests/Entity/ComputerTest.php
@@ -14,32 +14,32 @@ class ComputerTest extends \PHPUnit\Framework\TestCase
   public function testLaunchIntCode1()
   {
     $memory = "2,3,0,3,99";
-    $computer = Computer::createFromMemory($memory);
+    $computer = Computer::createFromMemoryString($memory);
     $this->assertEquals($computer->runIntCodeProgram(), "2");
   }
 
   public function testLaunchIntCode2()
   {
-    $computer = Computer::createFromMemory("2,3,0,3,99");
+    $computer = Computer::createFromMemoryString("2,3,0,3,99");
     $this->assertEquals($computer->runIntCodeProgram(), "2");
   }
 
   public function testLaunchIntCode3()
   {
     $memory = "1,9,10,3,2,3,11,0,99,30,40,50";
-    $computer = Computer::createFromMemory($memory);
+    $computer = Computer::createFromMemoryString($memory);
     $this->assertEquals("3500", $computer->runIntCodeProgram());
   }
 
   public function testLaunchIntCode4()
   {
-    $computer = Computer::createFromMemory("2,4,4,5,99,0");
+    $computer = Computer::createFromMemoryString("2,4,4,5,99,0");
     $this->assertEquals($computer->runIntCodeProgram(), "2");
   }
 
   public function testLaunchIntCode5()
   {
-    $computer = Computer::createFromMemory("1,1,1,4,99,5,6,0,99");
+    $computer = Computer::createFromMemoryString("1,1,1,4,99,5,6,0,99");
     $this->assertEquals($computer->runIntCodeProgram(), "30");
   }
 }
