11034c93e5eaeca1942d3d4b0c59038f1b192b90
Emilio Rodriguez <emilio.rodriguez@sngular.com>
2019-12-10 15:07:42 +0100

    day 5 first refactor

--- src/Domain/Entity/IntCodeInstructionFactory.php
+++ src/Domain/Entity/IntCodeInstructionFactory.php
@@ -16,24 +16,24 @@ class IntCodeInstructionFactory
   public static function fromMemory($memory, $pointer)
   {
     $opCode = intval(substr($memory[$pointer], -2, 2));
-    $param1Mode = self::getParamMode($memory[$pointer],1);
-    $param2Mode = self::getParamMode($memory[$pointer],2);
+    $param1Mode = self::getParamMode($memory[$pointer], 1);
+    $param2Mode = self::getParamMode($memory[$pointer], 2);
     switch ($opCode) {
       case self::OP_CODE_ADD:
-        $param1 = ($param1Mode === 0 ) ? $memory[$memory[$pointer + 1]]: $memory[$pointer + 1];
-        $param2 = ($param2Mode === 0 ) ? $memory[$memory[$pointer + 2]]: $memory[$pointer + 2];
+        $param1 = self::getParamValue($memory, $memory[$pointer + 1], $param1Mode);
+        $param2 = self::getParamValue($memory, $memory[$pointer + 2], $param2Mode);
         $resultPosition = $memory[$pointer + 3];
         return new IntCodeInstructionAdd($param1, $param2, $resultPosition);
       case self::OP_CODE_MULTIPLY:
-        $param1 = ($param1Mode === 0 ) ? $memory[$memory[$pointer + 1]]: $memory[$pointer + 1];
-        $param2 = ($param2Mode === 0 ) ? $memory[$memory[$pointer + 2]]: $memory[$pointer + 2];
-      $resultPosition = $memory[$pointer + 3];
+        $param1 = self::getParamValue($memory, $memory[$pointer + 1], $param1Mode);
+        $param2 = self::getParamValue($memory, $memory[$pointer + 2], $param2Mode);
+        $resultPosition = $memory[$pointer + 3];
         return new IntCodeInstructionMultiply($param1, $param2, $resultPosition);
       case self::OP_CODE_STORE:
         $param = $memory[$pointer + 1];
         return new IntCodeInstructionStore($param);
       case self::OP_CODE_OUTPUT:
-        $param = ($param1Mode === 0 ) ? $memory[$memory[$pointer + 1]]: $memory[$pointer + 1];
+        $param = self::getParamValue($memory, $memory[$pointer + 1], $param1Mode);
         return new IntCodeInstructionOutput($param);
       case self::OP_CODE_FINISHED:
         return new IntCodeInstructionFinish();
@@ -52,5 +52,10 @@ class IntCodeInstructionFactory
       return 1;
     }
     return 0;
+  }
+
+  private static function getParamValue($memory, $position, int $paramMode)
+  {
+    return ($paramMode === 0) ? $memory[$position] : $position;
   }
 }
